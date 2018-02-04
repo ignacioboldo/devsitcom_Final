015ed943dafbac908da49a716b164c9a892c39ea
Ignacio <ignacio.boldo@gmail.com>
2018-02-04 10:12:17 -0300

    Agrego ReportesController que había faltado para que funcione ok la integración con el Emi de reportes.

--- V 1.1/ProyectoFinal/ProyectoFinal/Controllers/ReportesController.cs
+++ V 1.1/ProyectoFinal/ProyectoFinal/Controllers/ReportesController.cs
@@ -5,83 +5,108 @@ using System.Linq;
 using System.Web;
 using System.Web.DynamicData;
 using System.Web.Mvc;
+using DAL;
 using BL;
 
 namespace ProyectoFinal.Controllers
 {
     public class ReportesController : Controller
     {
+        public UsuarioEntity usuarioActual;
+        public UsuariosManager um = new UsuariosManager();
+        public NegociosManager nm = new NegociosManager();
+        ReportesManager rm = new ReportesManager();
+
+
+        public List<Negocio> obtenerHospedajesPorUsuario()
+        {
+            var lista = nm.GetHospedajeByUsuario(usuarioActual.idUsuario);
+            return lista;
+        }
+        public void ObtenerUsuarioActual()
+        {
+            usuarioActual = (UsuarioEntity)Session["User"];
+        }
         // GET: Reportes
         public ActionResult IndexReportes()
         {
+            ObtenerUsuarioActual();
+
+            if (usuarioActual.idUsuario == 0)
+                usuarioActual.idPerfil = 0;
+
+            ViewBag.idPerfil = usuarioActual.idPerfil;
+            ViewBag.countComercio = nm.GetComercioByUsuario(usuarioActual.idUsuario).Count;
+            ViewBag.countHospedaje = nm.GetHospedajeByUsuario(usuarioActual.idUsuario).Count;
             return View();
+            
         }
-        //public ActionResult BuscarReporte(string nombre_reporte, DateTime? fecha_desde, DateTime? fecha_hasta)
-        //{
-        //    var vista_reporte = "";
-
-        //    switch (nombre_reporte)
-        //    {
-        //        case "GRAFICO_PIE":
-
-        //            vista_reporte = "GraficoPie";
-        //            break;
-        //        case "GRAFICO_BAR":
-
-        //            vista_reporte = "GraficoBar";
-        //            break;
-        //        case "DATA_TABLA":
-
-        //            var lista = new List<ReportesDataTableEntitiesFechaValor>();
-        //            //  ReportesDataTableEntities rDataTable = new ReportesDataTableEntities();
-        //            ReportesManager rm = new ReportesManager();
-        //            lista = rm.getReporteCampoFecha(Convert.ToDateTime("1/1/2017"), Convert.ToDateTime("1/1/2020"), "Diaria");
-        //            // rDataTable.descripcion = "Prueba";
-        //            //lista.Add(rDataTable);
-
-        //            ViewBag.nombre_reporte = "Solicitudes de Reserva por Negocio";
-        //            ViewBag.data = lista;
-
-        //            vista_reporte = "DataTable";
-        //            break;
-        //        default:
-        //            vista_reporte = "SinDatos";
-        //            break;
-        //    }
-
-        //    return PartialView(vista_reporte);
-        //}
 
 
-        public ActionResult CampoFechaValor_Index (string nombre_reporte)
+
+        public ActionResult CampoFechaValor_Index(string nombre_reporte)
         {
             ViewBag.nombre_reporte = nombre_reporte;
             return View();
-        
+
+        }
+
+        public ActionResult CampoFechaValorNegocio_Index(string nombre_reporte, int idTipoReporte)
+        {
+            ObtenerUsuarioActual();
+            var listaNegocio = new List<Negocio>();
+
+            if (idTipoReporte == 1)
+                listaNegocio = nm.GetHospedajeByUsuario(usuarioActual.idUsuario);
+            else if(idTipoReporte == 2)
+                listaNegocio = nm.GetComercioByUsuario(usuarioActual.idUsuario);
+
+            ViewBag.lista_negocios = listaNegocio;
+            ViewBag.nombre_reporte = nombre_reporte;
+            return View();
+
         }
 
         public ActionResult ReporteCampoFechaValor(string tipo_reporte, string nombre_reporte, String fecha_desde, String fecha_hasta)
         {
 
             var lista = new List<ReportesCampoFechaValor>();
-            ReportesManager rm = new ReportesManager();
-            
+            var vista_reporte = "";          
+
 
             switch (nombre_reporte)
             {
                 case "Reservas por Categoria":
                     lista = rm.ObtenerReservasPorCategoria(Convert.ToDateTime(fecha_desde), Convert.ToDateTime(fecha_hasta), tipo_reporte);
                     ViewBag.nombre_campo = "Categoria";
-                    ViewBag.nombre_valor = "Cantidad de Reservas";
+                    ViewBag.nombre_valor = "Porcentaje de Reservas";
+                    ViewBag.data = lista;
+                    ViewBag.clase_reporte = "ReportesCampoFechaValor";
+                    vista_reporte = "CampoFechaValor_Tabla";
                     break;
-                case "Reporte2":
-     
-                    break;
-                case "Reporte3":
+                case "Reservas por Origen":
+                        lista = rm.ObtenerReservasPorOrigen(Convert.ToDateTime(fecha_desde), Convert.ToDateTime(fecha_hasta), tipo_reporte);
+                        ViewBag.nombre_campo = "Provincia";
+                        ViewBag.nombre_valor = "Porcentaje de Reservas";
+                        ViewBag.data = lista;
+                        ViewBag.clase_reporte = "ReportesCampoFechaValor";
+                        vista_reporte = "CampoFechaValor_Tabla";
+                        break;
+
+
+                case "Promociones por Comercio":
+                    lista = rm.ObtenerPromocionesPorComercio(Convert.ToDateTime(fecha_desde), Convert.ToDateTime(fecha_hasta), tipo_reporte);
+                    ViewBag.nombre_campo = "Comercio";
+                    ViewBag.nombre_valor = "Porcentaje de Promociones";
+                    ViewBag.data = lista;
+                    ViewBag.clase_reporte = "ReportesCampoFechaValor";
+                    vista_reporte = "CampoFechaValor_Tabla";
+
                     break;
 
+                
                 default:
-              
+
                     break;
             }
 
@@ -89,16 +114,50 @@ namespace ProyectoFinal.Controllers
             ViewBag.nombre_reporte = nombre_reporte;
             ViewBag.tipo_reporte = tipo_reporte;
             
-            ViewBag.data = lista;
-
-            var vista_reporte = "CampoFechaValor_Tabla";
-
-           
-
             return PartialView(vista_reporte);
         }
 
+        public ActionResult ReporteCampoFechaValorPorNegocio(string tipo_reporte, string nombre_reporte, String fecha_desde, String fecha_hasta, string negocio)
+        {
+            
+            var lista = new List<ReportesCampoFechaValor>();
+            var vista_reporte = "";         
+               
+            switch (nombre_reporte)
+            {
+               
+                case "Reservas por Origen Negocio":
+                    lista = rm.ObtenerReservasPorOrigenPorNegocio(Convert.ToDateTime(fecha_desde), Convert.ToDateTime(fecha_hasta), tipo_reporte, Convert.ToInt32(negocio));
+                    ViewBag.nombre_campo = "Provincia";
+                    ViewBag.nombre_valor = "Porcentaje de Reservas";
+                    ViewBag.data = lista;
+                    ViewBag.clase_reporte = "ReportesCampoFechaValor";
+                
+                    vista_reporte = "CampoFechaValor_Tabla";
 
+                    break;
+
+                case "Promociones no Utilizadas Negocio":
+                    lista = rm.ObtenerPromocionesNoUtilizadasPorNegocio(Convert.ToDateTime(fecha_desde), Convert.ToDateTime(fecha_hasta), tipo_reporte, Convert.ToInt32(negocio));
+                    ViewBag.nombre_campo = "Utilizada";
+                    ViewBag.nombre_valor = "Promociones No Utilizadas";
+                    ViewBag.data = lista;
+                    ViewBag.clase_reporte = "ReportesFechaValor";
+
+                    vista_reporte = "CampoFechaValor_Tabla";
+
+                    break;
+                default:
+
+                    break;
+            }
+
+
+            ViewBag.nombre_reporte = nombre_reporte;
+            ViewBag.tipo_reporte = tipo_reporte;
+           
+            return PartialView(vista_reporte);
+        }
 
     }
 }
